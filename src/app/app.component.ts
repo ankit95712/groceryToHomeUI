5a4f9e0215d6a2b049e22c55b5db78c7b64992d8
ankit singh <ankitsingh6.9.1991@gmail.com>
2020-07-11 00:25:32 +0530

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'groceryToHome';
+  showLogin = true;
+  show(){
+    this.showLogin=!this.showLogin;
+
+  }
+
 }
