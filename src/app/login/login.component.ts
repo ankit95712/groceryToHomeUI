c443f3f6b87cba5f4246d639476a9f12e15868db
ankit singh <ankitsingh6.9.1991@gmail.com>
2020-07-11 16:20:51 +0530

    login button work

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+username:string;
+password:string
+successfull:string
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+ login(): void {
+ if (!!this.username && !! this.password){
+ this.successfull="successfull login"}
+else
+{
+this.successfull="input cannot be empty"}
   }
 
 }
