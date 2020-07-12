24ae91f9756abee0963691e8bf4e49c546acd29f
Kvivek <vivit1006117@gmail.com>
2020-07-13 01:51:33 +0530

    Some UI fix

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,9 +1,9 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
-import {LoginComponent} from "./login.component";
-import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule} from "@angular/forms";
+import {LoginComponent} from './login.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 import { EnterOtpComponent } from './enter-otp/enter-otp.component';
 import { CreatePasswordComponent } from './create-password/create-password.component';
 
