56d07455a14fc71b00d9f4d67632f2eeb36171f1
Kvivek <vivit1006117@gmail.com>
2020-07-20 00:44:57 +0530

    Vivek | UI for menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,82 +1,97 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MenuItem} from 'primeng/api';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
-     items: MenuItem[];
-     ngOnInit() {
+  items: MenuItem[];
 
-       this.items = [
-                 {
-                     label: 'Home',
-                     items: [{
-                             label: 'New',
-                             icon: 'pi pi-fw pi-plus',
-                             items: [
-                                 {label: 'Project'},
-                                 {label: 'Other'},
-                             ]
-                         },
-                         {label: 'Open'},
-                         {label: 'Quit'}
-                     ]
-                 },
-                 {
-                     label: 'About Us',
-                     icon: 'pi pi-fw pi-pencil',
-                     items: [
-                         {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-                         {label: 'Refresh', icon: 'pi pi-fw pi-refresh'},
-                     ]
-                 },
-                 {
-                     label: 'Products',
-                                      icon: 'pi pi-fw pi-list',
-                                      items: [
-                                          {label: 'Vegetables', icon: 'pi pi-fw pi-trash'},
-                                          {label: 'Snacks', icon: 'pi pi-fw pi-refresh'},
-                                          {label: 'Gourmet', icon: 'pi pi-fw pi-trash'},
-                                          {label: 'Dairy', icon: 'pi pi-fw pi-refresh'},
-                                          {label: 'Fruits', icon: 'pi pi-fw pi-refresh'},
-                                          {label: 'Beverages', icon: 'pi pi-fw pi-refresh'},
-                                          {label: 'Branded items', icon: 'pi pi-fw pi-refresh'},
+  ngOnInit() {
 
-                                            ]
-                  },
-                  {
-                     label: 'Feedback',
-                                       icon: 'pi pi-fw pi-list',
-                                       items: [
-                                           {label: 'Vegetables', icon: 'pi pi-fw pi-trash'},
-                                           {label: 'Snacks', icon: 'pi pi-fw pi-refresh'},
-                                           {label: 'Gourmet', icon: 'pi pi-fw pi-trash'},
-                                           {label: 'Dairy', icon: 'pi pi-fw pi-refresh'},
-                                           {label: 'Fruits', icon: 'pi pi-fw pi-refresh'},
-                                           {label: 'Beverages', icon: 'pi pi-fw pi-refresh'},
-                                           {label: 'Branded items', icon: 'pi pi-fw pi-refresh'},
-                                            ]
-                  },
-                  {
-                      label: 'Services',
-                                                             icon: 'pi pi-fw pi-list',
-                                                             items: [
-                                                             {label: 'Vegetables', icon: 'pi pi-fw pi-trash'},
-                                                             {label: 'Snacks', icon: 'pi pi-fw pi-refresh'},
-                                                             {label: 'Gourmet', icon: 'pi pi-fw pi-trash'},
-                                                             {label: 'Dairy', icon: 'pi pi-fw pi-refresh'},
-                                                             {label: 'Fruits', icon: 'pi pi-fw pi-refresh'},
-                                                             {label: 'Beverages', icon: 'pi pi-fw pi-refresh'},
-                                                             {label: 'Branded items', icon: 'pi pi-fw pi-refresh'}
-                                                                 ,
-                                                                  ]
-                  }
-]
-}
+    this.items = [
+      {
+        label: 'Home',
+        items: [{
+          label: 'New',
+          icon: 'pi pi-fw pi-plus',
+          items: [
+            {label: 'Project'},
+            {label: 'Other'},
+          ]
+        },
+          {label: 'Open'},
+          {label: 'Quit'}
+        ]
+      },
+      {
+        label: 'About Us',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+          {label: 'Refresh', icon: 'pi pi-fw pi-refresh'},
+        ]
+      },
+      {
+        label: 'Products',
+        icon: 'pi pi-fw pi-list',
+        items: [
+          {label: 'Vegetables', icon: 'pi pi-fw pi-trash'},
+          {label: 'Snacks', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Gourmet', icon: 'pi pi-fw pi-trash'},
+          {label: 'Dairy', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Fruits', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Beverages', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Branded items', icon: 'pi pi-fw pi-refresh'},
+
+        ]
+      },
+      {
+        label: 'Services',
+        icon: 'pi pi-fw pi-list',
+        items: [
+          {label: 'Vegetables', icon: 'pi pi-fw pi-trash'},
+          {label: 'Snacks', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Gourmet', icon: 'pi pi-fw pi-trash'},
+          {label: 'Dairy', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Fruits', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Beverages', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Branded items', icon: 'pi pi-fw pi-refresh'}
+        ]
+      },
+      {
+        label: 'Feedback',
+        icon: 'pi pi-fw pi-list',
+        items: [
+          {label: 'Vegetables', icon: 'pi pi-fw pi-trash'},
+          {label: 'Snacks', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Gourmet', icon: 'pi pi-fw pi-trash'},
+          {label: 'Dairy', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Fruits', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Beverages', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Branded items', icon: 'pi pi-fw pi-refresh'},
+        ]
+      },
+      {
+        label: 'Contact Us',
+        icon: 'pi pi-fw pi-list',
+        items: [
+          {label: 'Vegetables', icon: 'pi pi-fw pi-trash'},
+          {label: 'Snacks', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Gourmet', icon: 'pi pi-fw pi-trash'},
+          {label: 'Dairy', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Fruits', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Beverages', icon: 'pi pi-fw pi-refresh'},
+          {label: 'Branded items', icon: 'pi pi-fw pi-refresh'},
+        ]
+      }
+    ];
+  }
 }
 
