761e71fb7644170e5577df7ee44a994624ce0c3f
Kvivek <vivit1006117@gmail.com>
2020-07-31 00:24:27 +0530

    Vivek | Fix: lint

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,8 +6,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormComponent } from './form/form.component';
-import {AboutUsModule} from "./about-us/about-us.module";
-import {SharedModule} from "./shared/shared.module";
+import {AboutUsModule} from './about-us/about-us.module';
+import {SharedModule} from './shared/shared.module';
 
 
 @NgModule({
