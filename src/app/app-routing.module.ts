a7e1b84d2f8f0705c3b142df784c398693980b86
ankit <ankitsingh6.9.1991@gmail.com>
2020-07-30 22:43:38 +0530

    form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {HomeComponent} from "./home/home.component";
+import {FormComponent} from "./home/form/form.component";
 import {ForgotPasswordComponent} from "./login/forgot-password/forgot-password.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'changePassword', component: ForgotPasswordComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'form', component: FormComponent }
 ];
 
 @NgModule({
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,10 +5,11 @@ import {HomeComponent} from './home.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormComponent } from './form/form.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, FormComponent],
   imports: [
     CommonModule,
     MenubarModule,
