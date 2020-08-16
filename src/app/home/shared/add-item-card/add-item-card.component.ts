5fee1be8f666a5212cd0b9be58f465e3cb5284ef
ankit <ankitsingh6.9.1991@gmail.com>
2020-08-16 23:29:07 +0530

    add item

--- src/app/home/shared/add-item-card/add-item-card.component.ts
+++ src/app/home/shared/add-item-card/add-item-card.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import {basicCardData} from '../../stubs/basicCard.stubs';
 @Component({
   selector: 'app-add-item-card',
   templateUrl: './add-item-card.component.html',
   styleUrls: ['./add-item-card.component.css']
 })
 export class AddItemCardComponent implements OnInit {
-
+display = false;
+basicCardDetails = basicCardData[0];
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  closeForm(): void {
+}
 }
